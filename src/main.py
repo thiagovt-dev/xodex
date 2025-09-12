89a19cbf4c17644e5adc3fde231f425fce3810b3
thiagovt-dev <thiagovt.dev@gmail.com>
2025-09-12 13:39:48 -0300

    chore: remove pasta src duplicada (usar pacote xodex)

--- src/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import asyncio
-from src.repl import start_repl
-from src.config import cfg
-
-print(f"Iniciando CLI com provider: {cfg['provider']}")
-asyncio.run(start_repl())
